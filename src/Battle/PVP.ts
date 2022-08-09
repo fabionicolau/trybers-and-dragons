a42a5082a9f273b61e985ad7bb7d2f3b4a1e5044
fabionicolau <fabionicolaujr@gmail.com>
2022-08-09 19:00:44 -0300

    feat: cria a classe PVP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,30 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  private _player1: Fighter;
+  private _player2: Fighter;
+
+  constructor(player1: Fighter, player2: Fighter) {
+    super(player1);
+    this._player1 = player1;
+    this._player2 = player2;
+  }
+
+  get player1(): Fighter {
+    return this._player1;
+  }
+
+  get player2(): Fighter {
+    return this._player2;
+  }
+
+  fight(): number {
+    while (this._player1.lifePoints > 0 && this._player2.lifePoints > 0) {
+      this._player1.attack(this._player2);
+      this._player2.attack(this._player1);
+    }
+   
+    return this._player1.lifePoints === -1 ? -1 : 1;
+  }
+}
