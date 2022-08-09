e0f9be1048cc9c27f4159a0a86beb71bc85dc644
fabionicolau <fabionicolaujr@gmail.com>
2022-08-09 18:03:53 -0300

    feat: cria a interface "SimpleFighter" e altera a interface "fighter" para estender a interface "SimpleFighter"

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,12 +1,10 @@
 import Energy from '../Energy';
+import SimpleFighter from './SimpleFighter';
 
-export default interface Fighter {
-  lifePoints: number;
-  strength: number;
+export default interface Fighter extends SimpleFighter {
   defense: number;
   energy?: Energy;
   attack(fighter: Fighter): void; 
   special?(fighter: Fighter): void; 
   levelUp(): void;
-  receiveDamage(attackPoints: number): number;
 }
