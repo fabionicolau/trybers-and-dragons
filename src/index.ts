5a73777ae1fe68ab215dac35753695bd85b3142b
fabionicolau <fabionicolaujr@gmail.com>
2022-08-10 00:01:10 -0300

    feat: cria objetos no arquivo index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
-import Race from './Races/Race';
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
 
-export default Race;
+const player1 = new Character('Legolas');
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+
+const player2 = new Character('Aragorn');
+const player3 = new Character('Gandalf');
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+const pvp = new PVP(player2, player3);
+
+const pve = new PVE(player1, [monster1, monster2]);
+
+function runBattles(battles: Battle[]): void {
+  battles.forEach((battle) => {
+    battle.fight();
+  });
+}
+
+export { player1, player2, player3, monster1, monster2, pvp, pve, runBattles };
